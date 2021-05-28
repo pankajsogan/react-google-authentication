c18568d064a955a9b115e394e44b426d992bdd3d
Pankaj Sogan <81852236+pankajsogan@users.noreply.github.com>
2021-05-28 07:05:40 +0530

    Update LoginHooks.js

--- src/components/LoginHooks.js
+++ src/components/LoginHooks.js
@@ -5,7 +5,7 @@ import { useGoogleLogin } from 'react-google-login';
 import { refreshTokenSetup } from '../utils/refreshToken';
 
 const clientId =
-  '707788443358-u05p46nssla3l8tmn58tpo9r5sommgks.apps.googleusercontent.com';
+  '688025704310-mal2da29j49vmat22s1ad156fg6a93f8.apps.googleusercontent.com';
 
 function LoginHooks() {
   const onSuccess = (res) => {
